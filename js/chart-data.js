867099609eec55d16311ef8a031b5e9fd29fb458
eabiii <edbar_10@yahoo.com>
2019-03-06 06:41:13 +0800

    design

--- js/chart-data.js
+++ js/chart-data.js
@@ -1,32 +1,5 @@
-var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
-	
-	var lineChartData = {
-		labels : ["January","February","March","April","May","June","July"],
-		datasets : [
-			{
-				label: "My First dataset",
-				fillColor : "rgba(220,220,220,0.2)",
-				strokeColor : "rgba(220,220,220,1)",
-				pointColor : "rgba(220,220,220,1)",
-				pointStrokeColor : "#fff",
-				pointHighlightFill : "#fff",
-				pointHighlightStroke : "rgba(220,220,220,1)",
-				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-			},
-			{
-				label: "My Second dataset",
-				fillColor : "rgba(48, 164, 255, 0.2)",
-				strokeColor : "rgba(48, 164, 255, 1)",
-				pointColor : "rgba(48, 164, 255, 1)",
-				pointStrokeColor : "#fff",
-				pointHighlightFill : "#fff",
-				pointHighlightStroke : "rgba(48, 164, 255, 1)",
-				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-			}
-		]
 
-	}
-		
+/*
 	var barChartData = {
 		labels : ["January","February","March","April","May","June","July"],
 		datasets : [
@@ -47,114 +20,114 @@ var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
 		]
 
 	}
+	*/
 
-	var pieData = [
+	var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [{
+        label: 'Personalå',
+        data: [10, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 99, 132, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(255,99,132,1)',
+          'rgba(255,99,132,1)',
+          'rgba(255,99,132,1)',
+          'rgba(255,99,132,1)',
+          'rgba(255,99,132,1)'
+        ],
+        borderWidth: 2
+      },
+      {
+        label: '# of Votes 2',
+        data: [15, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 2
+      },
 			{
-				value: 300,
-				color:"#30a5ff",
-				highlight: "#62b9fb",
-				label: "Blue"
-			},
+        label: '# of Votes 2',
+        data: [15, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 2
+      },
 			{
-				value: 50,
-				color: "#ffb53e",
-				highlight: "#fac878",
-				label: "Orange"
-			},
-			{
-				value: 100,
-				color: "#1ebfae",
-				highlight: "#3cdfce",
-				label: "Teal"
-			},
-			{
-				value: 120,
-				color: "#f9243f",
-				highlight: "#f6495f",
-				label: "Red"
-			}
+        label: '# of Votes 2',
+        data: [15, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 2
+      }
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        stacked: true,
+        ticks: {
+          beginAtZero: true
+        }
+      }],
+      xAxes: [{
+        stacked: true,
+        ticks: {
+          beginAtZero: true
+        }
+      }]
 
-		];
-			
-	var doughnutData = [
-				{
-					value: 300,
-					color:"#30a5ff",
-					highlight: "#62b9fb",
-					label: "Blue"
-				},
-				{
-					value: 50,
-					color: "#ffb53e",
-					highlight: "#fac878",
-					label: "Orange"
-				},
-				{
-					value: 100,
-					color: "#1ebfae",
-					highlight: "#3cdfce",
-					label: "Teal"
-				},
-				{
-					value: 120,
-					color: "#f9243f",
-					highlight: "#f6495f",
-					label: "Red"
-				}
-
-			];
-			
-	var radarData = {
-	    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
-	    datasets: [
-	        {
-	            label: "My First dataset",
-	            fillColor: "rgba(220,220,220,0.2)",
-	            strokeColor: "rgba(220,220,220,1)",
-	            pointColor: "rgba(220,220,220,1)",
-	            pointStrokeColor: "#fff",
-	            pointHighlightFill: "#fff",
-	            pointHighlightStroke: "rgba(220,220,220,1)",
-	            data: [65, 59, 90, 81, 56, 55, 40]
-	        },
-	        {
-	            label: "My Second dataset",
-	            fillColor : "rgba(48, 164, 255, 0.2)",
-	            strokeColor : "rgba(48, 164, 255, 0.8)",
-	            pointColor : "rgba(48, 164, 255, 1)",
-	            pointStrokeColor : "#fff",
-	            pointHighlightFill : "#fff",
-	            pointHighlightStroke : "rgba(48, 164, 255, 1)",
-	            data: [28, 48, 40, 19, 96, 27, 100]
-	        }
-	    ]
-	};
-	
-	var polarData = [
-		    {
-		    	value: 300,
-		    	color: "#1ebfae",
-		    	highlight: "#38cabe",
-		    	label: "Teal"
-		    },
-		    {
-		    	value: 140,
-		    	color: "#ffb53e",
-		    	highlight: "#fac878",
-		    	label: "Orange"
-		    },
-		    {
-		    	value: 220,
-		    	color:"#30a5ff",
-		    	highlight: "#62b9fb",
-		    	label: "Blue"
-		    },
-		    {
-		    	value: 250,
-		    	color: "#f9243f",
-		    	highlight: "#f6495f",
-		    	label: "Red"
-		    }
-		
-	];
-
+    }
+  }
+});
